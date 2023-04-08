8b365a6d6a2d962e51b9b225a65b82710dce67ee
ByMortt <121973352+ByMortt@users.noreply.github.com>
2023-04-07 23:06:15 -0600

    codigo fuente

---
+++ src/unam/fi/poo/g8/MiClase.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unam.fi.poo.g8;
+
+/**
+ * Este programa imprime un Hola mundo
+ * @author poo08alu03
+ */
+public class MiClase {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    /**
+     * Este metodo reliza la multiplicacion de un entero por un flotante y retorna un double 
+     * @param a Parametro de tippo entero
+     * @param b Parametro de tipo flotante
+     * @return  un valor de tipo double
+     */
+    public static double metodo(double a, float b){
+        return a*b;
+    }
+}
